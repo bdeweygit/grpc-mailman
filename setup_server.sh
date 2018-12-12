c9eb8bdc158c18b1460d45d7d53178e9da8c8e39
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-12-12 16:07:38 -0500

    consolidate setup scripts into one

--- setup_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set +x
-virtualenv -p python3 env
-source ./env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. mailbox.proto
-python3 server.py &
