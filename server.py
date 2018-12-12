f0eff76a738baef10b990f2034da8b6f07749a64
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-12-12 02:03:30 -0500

    implement RegisterMailbox

--- server.py
+++ server.py
@@ -1,16 +1,38 @@
 from concurrent import futures
 import time
+import random
+import string
 import grpc
 
 import mailbox_pb2
 import mailbox_pb2_grpc
 
 ONE_DAY_IN_SECONDS = 60 * 60 * 24
+PASSWORD_LENGTH = 8
+
+class Mailbox():
+    def __init__(self, name):
+        self.NAME = name
+        self.PASSWORD = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(PASSWORD_LENGTH))
 
 
 class MailMan(mailbox_pb2_grpc.MailManServicer):
+    def __init__(self):
+        self.mailboxes = {}
+
     def RegisterMailbox(self, request, context):
-        pass
+        name = request.name
+
+        response = mailbox_pb2.RegisterMailboxReply()
+        if name in self.mailboxes:
+            response.error = 'mailbox name is already taken'
+        else:
+            mailbox = Mailbox(name=name)
+            self.mailboxes[name] = mailbox
+            response.password = mailbox.PASSWORD
+
+        return response
+
 
     def RemoveMailbox(self, request, context):
         pass
